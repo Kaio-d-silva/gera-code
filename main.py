111c6d581a5b4dc7d94bbfb82fc685e0e85b3faa
root <root@DESKTOP-SL4V7UT>
2024-11-09 22:15:06 -0400

    parte de ui feita e funcional, partindo para geração de projeto

--- main.py
+++ main.py
@@ -1,53 +1,57 @@
-# from get_path_directory import get_path_directory
-# from check_path_folder import check_path, go_to, checks_files
-# from user_input import user_input_dir,options,select_project
-from comand_bash import *
-from maneger_dir import *
+# # from get_path_directory import get_path_directory
+# # from check_path_folder import check_path, go_to, checks_files
+# # from user_input import user_input_dir,options,select_project
+# from comand_bash import *
+# from maneger_dir import *
 
-# VERIFICA QUAL É O DIRETORIO PADRAO
-project_diretory = get_path_directory()
+# # VERIFICA QUAL É O DIRETORIO PADRAO
+# project_diretory = get_path_directory()
 
-# VERIFICA SE EXISTE O DIRETÓRIO PADRÃO CASO NÃO ELE O CRIA
-check_path(project_diretory)
+# # VERIFICA SE EXISTE O DIRETÓRIO PADRÃO CASO NÃO ELE O CRIA
+# check_path(project_diretory)
 
-# VAI PARA DIRETORIO DO PROJETO 
-go_to(project_diretory)
+# # VAI PARA DIRETORIO DO PROJETO 
+# go_to(project_diretory)
 
-# SOLICITA AO USUÁRIO UM NOME PARA O PROJETO A SER CRIADO
-path_full_new_project = user_input_dir(project_diretory)
+# # SOLICITA AO USUÁRIO UM NOME PARA O PROJETO A SER CRIADO
+# path_full_new_project = user_input_dir(project_diretory)
 
-# VERIFICA SE EXISTE O DIRETÓRIO PADRÃO CASO NÃO ELE O CRIA
-check_path(path_full_new_project)
+# # VERIFICA SE EXISTE O DIRETÓRIO PADRÃO CASO NÃO ELE O CRIA
+# check_path(path_full_new_project)
 
-# ENTRA NO DIRETÓRIO
-go_to(path_full_new_project)
+# # ENTRA NO DIRETÓRIO
+# go_to(path_full_new_project)
 
-# VERIFICA SE NA RAIZ DO PROJETO EXISTE UM ARQUIVO MAIN
-# CASO NÃO ELE É CRIADO
+# # VERIFICA SE NA RAIZ DO PROJETO EXISTE UM ARQUIVO MAIN
+# # CASO NÃO ELE É CRIADO
 
-#checks_files(path_full_new_project)
+# #checks_files(path_full_new_project)
 
 
 
-#upgrad_linux()
-selected_project = select_project()
+# #upgrad_linux()
+# selected_project = select_project()
 
-if selected_project == "Python":
-    install_pip()
-    creat_virtual_venv()
-    user_option = options()
-    if user_option == "SIM":
-        install_tkinter() 
-    else:
-        print("Projeto criado sem Tkinter")
+# if selected_project == "Python":
+#     install_pip()
+#     creat_virtual_venv()
+#     user_option = options()
+#     if user_option == "SIM":
+#         install_tkinter() 
+#     else:
+#         print("Projeto criado sem Tkinter")
         
-elif selected_project == "Node":
-    change_dir_home()
-    install_node()
-    create_react_project(path_full_new_project)
+# elif selected_project == "Node":
+#     change_dir_home()
+#     install_node()
+#     create_react_project(path_full_new_project)
      
     
 
-open_vscode(path_full_new_project)
+# open_vscode(path_full_new_project)
 
+from ui_classe import App
 
+if __name__ == "__main__":
+    app = App()
+    app.run()
---
+++ ui_classe.py
@@ -0,0 +1,63 @@
+import tkinter as tk 
+from tkinter import ttk
+from tkinter import *
+from handlers import validate_input
+
+class App():
+    def __init__(self):
+        self.window = tk.Tk()
+        self.window.title("gerador de projetos")
+        
+        self.creat_widgets()
+        
+    def creat_widgets(self):
+        # Primeira label
+        self.label_entry = Label(self.window, text="Digite o nome do projeto sem espaços e acentuação:")
+        self.label_entry.pack(pady=5)
+        
+        # Entrada o usuario
+        self.language_entry = Entry()
+        self.language_entry.pack(pady=5)
+
+        # Segunda label
+        self.label_select_langue = Label(self.window, text="selecione uma linguagem")
+        self.label_select_langue.pack(pady=5)
+
+        # Seleção da linguagem
+        self.check_box = ttk.Combobox(self.window, values=["Python","Node"])
+        self.check_box.pack(pady=5) 
+
+        # Botão Enviar
+        self.send_buton = Button(self.window, text="Enviar", command=self.display_route)
+        self.send_buton.pack(pady=5)
+
+        # Botão salvar
+        self.generate_button = Button(self.window, text="Gerar", command=self.generate_project)
+        self.generate_button.pack(pady=5)
+        
+         # terceira label teste
+        self.result_label = Label(self.window,text="")
+        self.result_label.pack(pady=5)
+
+        
+    def run(self):
+        self.window.mainloop()
+        
+    def generate_project(self):
+        name = self.language_entry.get()
+        language = self.check_box.get()
+        
+        
+        
+    def display_route(self):
+        name = self.language_entry.get()
+        language = self.check_box.get()
+        
+        if validate_input(name):
+            self.result_label.config(text=f"{name}, {language}")
+        else:
+            self.result_label.config(text="Nome invalido")
+
+                
+            
+            
