95641ba60e39cce645922b097667f328b471929f
Kaio-d-silva <kaio54220266@aluno.ms.senac.br>
2024-11-11 19:31:27 -0400

    UI (#1)
    * criando branche
    
    * alteração no ui
    
    * parte de ui feita e funcional, partindo para geração de projeto
    
    * fazendo ponte entre botão de gerar e a parte de fabricar o projeto
    
    * mudei o nome do ui classe
    
    * Parte da geração de projeto esta pronta, preciso aplicar mostrar os comandos do terminal na tela do tkinter
    
    ---------
    
    Co-authored-by: root <root@DESKTOP-SL4V7UT>

---
+++ project_factory.py
@@ -0,0 +1,26 @@
+from comand_bash import *
+from maneger_dir import go_to, check_path, check_file
+
+def make_python_project(path_directory,name_project):
+    path_full_new_project = f"{path_directory}/{name_project}"
+    check_path(path_full_new_project)
+    go_to(path_full_new_project)
+    check_file(path_full_new_project)
+    user_option = input("Quer instar o tkinter ? ")
+    install_pip()
+    creat_virtual_venv()
+    if user_option == "SIM" or user_option == "sim" or user_option == "S" or user_option == "s":
+        install_tkinter() 
+    else:
+        print("Projeto criado sem Tkinter")
+    return path_full_new_project
+    
+def make_node_project(path_directory,name_project):
+    path_full_new_project = f"{path_directory}/{name_project}"
+    change_dir_home()
+    install_node()
+    go_to(path_directory)
+    create_react_project(name_project)
+    return path_full_new_project
+    
+    
---
+++ ui.py
@@ -0,0 +1,71 @@
+import tkinter as tk 
+from tkinter import ttk
+from tkinter import *
+from handlers import validate_input, save_data
+from comand_bash import open_vscode
+
+class App():
+    def __init__(self):
+        self.window = tk.Tk()
+        self.window.title("gerador de projetos")
+        
+        self.creat_widgets()
+        
+    def creat_widgets(self):
+        # Primeira label
+        self.label_entry = Label(self.window, text="Digite o nome do projeto sem espaços e acentuação:")
+        self.label_entry.pack(pady=5)
+        
+        # Entrada o usuario
+        self.language_entry = Entry()
+        self.language_entry.pack(pady=5)
+
+        # Segunda label
+        self.label_select_langue = Label(self.window, text="selecione uma linguagem")
+        self.label_select_langue.pack(pady=5)
+
+        # Seleção da linguagem
+        self.check_box = ttk.Combobox(self.window, values=["Python","Node"])
+        self.check_box.pack(pady=5) 
+
+        # Botão Enviar
+        self.send_buton = Button(self.window, text="Enviar", command=self.display_route)
+        self.send_buton.pack(pady=5)
+
+        # Botão salvar
+        self.generate_button = Button(self.window, text="Gerar", command=self.generate_project)
+        self.generate_button.pack(pady=5)
+        
+         # terceira label teste
+        self.result_label = Label(self.window,text="")
+        self.result_label.pack(pady=5)
+
+        
+    def run(self):
+        self.window.mainloop()
+        
+    def generate_project(self):
+        name_project = self.language_entry.get()
+        language = self.check_box.get()
+        
+        if validate_input(name_project):
+            path_full_new_project = save_data(name_project, language)
+            mensage = f"Projeto foi criado em {path_full_new_project}"
+            self.result_label.config(text=mensage)
+            open_vscode(path_full_new_project)
+        else:
+            self.result_label.config(text="Erro : Nome invalido")
+            
+        
+    def display_route(self):
+        name = self.language_entry.get()
+        language = self.check_box.get()
+        
+        if validate_input(name):
+            self.result_label.config(text=f"{name}, {language}")
+        else:
+            self.result_label.config(text="Nome invalido")
+
+                
+            
+            
